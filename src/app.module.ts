e934ff222321c1eaefbbabd6c417db1d2597bad1
Feisal Mohamed <feisalmib@gmail.com>
2025-03-04 10:16:52 +0300

    coonecting postrgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,26 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { PostsModule } from './posts/posts.module';
+import {TypeOrmModule} from '@nestjs/typeorm';
+
 
 @Module({
-  imports: [UsersModule, AuthModule, PostsModule],
+  imports: [UsersModule, AuthModule, PostsModule,
+    TypeOrmModule.forRootAsync({
+
+      useFactory:() =>({
+        type:'postgres',
+        entities:[],
+        synchronize:true,
+        port:5432,
+        username:'postgres',
+        password:'root',
+        host:'localhost',
+        database:'nest'
+      })
+      
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
